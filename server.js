9287a198a036be9b151028d8b69ef7aa5fec92ae
sahith <honey.sahith29@gmail.com>
2023-11-15 12:17:32 +0530

    added text

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hi");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hi");
+// });
 
+app.get("/" ,  (req, res) =>{
+    res.sendFile(path.join(__dirname, "/index.html"));
+})
 
 app.listen(port, () => {
     console.log(`app running on port `, port)
