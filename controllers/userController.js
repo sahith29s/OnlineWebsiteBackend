510db1f214e56bbf74201e7d2740fa088ac33bf5
sahith <honey.sahith29@gmail.com>
2023-11-24 10:28:48 +0530

    debug

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,41 +4,42 @@ const jwt = require("jsonwebtoken");
 const createUser = async (req, res) => {
     try {
         const { username, profile, email, password } = req.body;
-            console.log("normal");
-            console.log("username ", username);
-            console.log("profile ", profile);
-            console.log("email ", email);
-            console.log("password ", password);
+        console.log("normal");
+        console.log("username ", username);
+        console.log("profile ", profile);
+        console.log("email ", email);
+        console.log("password ", password);
 
-        if (await userModel.findOne({ email })) {
-            console.log("await findOne");
-            console.log("username ", username);
-            console.log("profile ", profile);
-            console.log("email ", email);
-            console.log("password ", password);
-            res.status(409).json({ message: "user already created" });
-            return;
-        }
-        if (profile) {
-            console.log("if profile");
-            console.log("username ", username);
-            console.log("profile ", profile);
-            console.log("email ", email);
-            console.log("password ", password);
-            const newUser = new userModel({ username, profile, email, password });
-            await newUser.save();
-            res.status(201).json(newUser);
-        }
-        else {
-            console.log("else");
-            console.log("username ", username);
-            console.log("profile ", profile);
-            console.log("email ", email);
-            console.log("password ", password);
-            const newUser = new userModel({ username, email, password });
-            await newUser.save();
-            res.status(201).json(newUser);
-        }
+        // if (await userModel.findOne({ email })) {
+        //     console.log("await findOne");
+        //     console.log("username ", username);
+        //     console.log("profile ", profile);
+        //     console.log("email ", email);
+        //     console.log("password ", password);
+        //     res.status(409).json({ message: "user already created" });
+        //     return;
+        // }
+        // if (profile) {
+        //     console.log("if profile");
+        //     console.log("username ", username);
+        //     console.log("profile ", profile);
+        //     console.log("email ", email);
+        //     console.log("password ", password);
+        //     const newUser = new userModel({ username, profile, email, password });
+        //     await newUser.save();
+        //     res.status(201).json(newUser);
+        // }
+        // else {
+        console.log("username ", username);
+        console.log("profile ", profile);
+        console.log("email ", email);
+        console.log("password ", password);
+        const newUser = new userModel({ username, email, password });
+        console.log("Before model" , newUser);
+        await newUser.save();
+        console.log("Ater model", newUser);
+        res.status(201).json(newUser);
+        // }
     }
     catch (error) {
         res.json(error);
