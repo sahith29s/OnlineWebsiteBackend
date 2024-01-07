eabaefddb7c6ad786cb18970e3ff17cdae325541
sahith <honey.sahith29@gmail.com>
2024-01-07 18:41:37 +0530

    added nodemailer

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,48 +3,48 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
-    try {
-        let { username, profile, email, password } = req.body;
+  try {
+    let { username, profile, email, password } = req.body;
 
-        if (await userModel.findOne({ email })) {
-            res.status(409).json({ message: "user already created" });
-            return;
-        }
-        if (profile) {
-            password = await bcrypt.hash(password, 10);
-            console.log(password)
-            const newUser = userModel.create({ username, password, profile, email });
-            res.status(201).json(newUser);
-        }
-        else {
-            password = await bcrypt.hash(password, 10);
-            const newUser = await userModel.create({ username, email, password });
-            console.log("Ater model", newUser);
-            res.status(201).json(newUser);
-        }
+    if (await userModel.findOne({ email })) {
+      res.status(409).json({ message: "user already created" });
+      return;
     }
-    catch (error) {
-        res.json(error);
-    };
+    if (profile) {
+      password = await bcrypt.hash(password, 10);
+      console.log(password)
+      const newUser = userModel.create({ username, password, profile, email });
+      res.status(201).json(newUser);
+    }
+    else {
+      password = await bcrypt.hash(password, 10);
+      const newUser = await userModel.create({ username, email, password });
+      console.log("Ater model", newUser);
+      res.status(201).json(newUser);
+    }
+  }
+  catch (error) {
+    res.json(error);
+  };
 }
 
 const LoginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await userModel.findOne({ email });
-        if (user && await bcrypt.compare(password, user.password)) {
-            user.password = "";
-            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-            console.log(token);
-            res.status(200).json([user, token]);
-        }
-        else {
-            res.status(404).json({ message: "User not found please sign up first" });
-        }
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (user && await bcrypt.compare(password, user.password)) {
+      user.password = "";
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+      console.log(token);
+      res.status(200).json([user, token]);
     }
-    catch (error) {
-        console.log(error);
-    };
+    else {
+      res.status(404).json({ message: "User not found please sign up first" });
+    }
+  }
+  catch (error) {
+    console.log(error);
+  };
 }
 
 const getUser = async (req, res) => {
