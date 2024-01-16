e5a80337d519d7387300cf0947f00410ac622b46
sahith <honey.sahith29@gmail.com>
2024-01-16 21:18:35 +0530

    removed error

--- routes/nodeMailerRoutes.js
+++ routes/nodeMailerRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const contactModel = require("../models/contactModel");
 const { main } = require("../utils/nodemailer");
 router.post("/", async (req, res) => {
-  const {email , text , name} = req.body;
-  contactModel.create({name , email , text});
-  main(email , text , name)
-  res.send("done")
+  main(email , text , name);
+  res.send("done");
 });
 
 module.exports = router
